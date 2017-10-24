bd7fdbb35ae38e3aaf5ce3983b8ace5de5666d92
Alberto Valverde <alberto@meteogrid.com>
2017-10-24 17:13:56 +0200

    language-c from github until it lands on nixpkgs to avoid downloading the enormouns all-cabal-hashes

--- release.nix
+++ release.nix
@@ -2,21 +2,11 @@ let
   revision = "36a4dc392a40e06973a64b7666b179f4a021682a";
   nixpkgsUrl =
     "https://github.com/NixOS/nixpkgs-channels/archive/${revision}.tar.gz";
-  pkgs = import (fetchTarball nixpkgsUrl) {
-     config = {
-       packageOverrides = super: {
-         # Needed for language-c 0.7. Remove once nixpkgs includes it
-         all-cabal-hashes = super.fetchFromGitHub {
-           owner = "commercialhaskell";
-           repo = "all-cabal-hashes";
-           rev = "1ece7c6737619c9720f0c8427e9a2d12a7b316e5";
-           sha256 = "0qbjm81ql4g3cadly15mp0xljrbnymp26n44312krywbanvjdxdl";
-         };
-       };
-     };
-  };
+  pkgs = import (fetchTarball nixpkgsUrl) {};
+
   hsLib = import "${pkgs.path}/pkgs/development/haskell-modules/lib.nix"
             { inherit pkgs; inherit (pkgs) lib;};
+
   # Need this branch of c2hs until a release supports language-c 0.7, see
   # https://github.com/haskell/c2hs/issues/194
   c2hsSrc = pkgs.fetchFromGitHub {
@@ -25,11 +15,21 @@ let
     rev = "8b79823c32e234c161baec67fdf7907952ca62b8";
     sha256 = "0hyrcyssclkdfcw2kgcark8jl869snwnbrhr9k0a9sbpk72wp7nz";
   };
+
+  # we need language-c 0.7 for the fix to
+  # https://github.com/visq/language-c/issues/34.
+  # From github until the hash from the hackage release
+  # lands in nixpkgs all-cabal-hashes
+  languageCsrc = pkgs.fetchFromGitHub {
+    owner = "visq";
+    repo = "language-c";
+    rev = "54dfce30add1f0ac1d55413ecf59e5f08a49258d";
+    sha256 = "0ilfd73336745k7f58vmvrm5j06z977g7pjrl9pdx6fbx3m3aqyb";
+  };
+
   haskellPackages = pkgs.haskell.packages.ghc802.override {
     overrides = self: super: {
-      # we need language-c 0.7 for the fix to
-      # https://github.com/visq/language-c/issues/34
-      language-c = self.callHackage "language-c" "0.7.0" {};
+      language-c = self.callCabal2nix "language-c" languageCsrc {};
       c2hs = hsLib.dontCheck (super.callCabal2nix "c2hs" c2hsSrc {});
     };
   };
