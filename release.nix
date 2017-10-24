c8e5c08c5df8aa2c40e3b49166b7cdcdb1bca86c
Alberto Valverde <alberto@toscat.net>
2017-10-24 12:31:10 +0200

    update nixpkgs to latest nixos-17.09 and use GHC 8.2.1

--- release.nix
+++ release.nix
@@ -1,11 +1,11 @@
 let
-  revision = "81fceb255448415e70b9e7775d590b6def45f861";
+  revision = "36a4dc392a40e06973a64b7666b179f4a021682a";
   nixpkgsUrl =
     "https://github.com/NixOS/nixpkgs-channels/archive/${revision}.tar.gz";
   pkgs = import (fetchTarball nixpkgsUrl) {};
   withGdal = gdal:
     let
-      packages = pkgs.haskell.packages.ghc802;
+      packages = pkgs.haskell.packages.ghc821;
     in packages.callPackage ./.  { inherit gdal; };
 
   jobs = rec {
